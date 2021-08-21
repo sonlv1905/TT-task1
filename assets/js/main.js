7ea13311889af460057138ea4d11c7fb6bcd5cdc
sonlv1905 <80377041+sonlv1905@users.noreply.github.com>
2021-08-21 16:20:49 +0700

    update error

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,13 @@ var close = document.getElementById("close");
 var modal = document.getElementById("header__modal");
 toggle.onclick = function() {
     modal.style.display='block';
-    close.style.display='block';
+    close.style.display='flex';
     toggle.style.display='none';
 };
 
 close.onclick = function() {
     modal.style.display='none';
     close.style.display='none';
-    toggle.style.display='block';
+    toggle.style.display='flex';
 }
 
